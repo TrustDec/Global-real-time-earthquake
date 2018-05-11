4c15945fa6dae13ba3fdca8f0194be496101ccf1
Small Rui <603049583@qq.com>
2018-05-12 02:14:50 +0800

    hello world

--- Hello.py
+++ Hello.py
@@ -1,3 +1,4 @@
 import requests
-res = requests.get("http://news.sina.com.cn/china/")
-print(res.encoding)
+res = requests.get("http://www.ceic.ac.cn/ajax/speedsearch?num=6&&page=1")
+res.encoding = 'utf-8';
+print(res.text)
