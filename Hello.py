c87bff70e593ba085187cbb1f723b60e91d913c0
Small Rui <603049583@qq.com>
2018-05-12 02:20:37 +0800

    download douban top250 /install BeautifulSoup

--- Hello.py
+++ Hello.py
@@ -1,4 +1,16 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
 import requests
-res = requests.get("http://www.ceic.ac.cn/ajax/speedsearch?num=6&&page=1")
-res.encoding = 'utf-8';
-print(res.text)
+DOWNLOAD_URL = 'http://movie.douban.com/top250'
+
+def download_page(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36'
+    }
+    data = requests.get(url).content
+    return data
+def main():
+    print download_page(DOWNLOAD_URL)
+if __name__ == '__main__':
+    main()
